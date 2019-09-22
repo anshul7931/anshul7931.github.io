1dde00f120dd327c6d97a4a7dfc0bf7500d51376
Anshul Goel <KH2280@kitspl.com>
2019-09-22 08:32:02 +0530

    Added fabreact

---
+++ fabreact/precache-manifest.1a5f9b1d86db45ec442afd74ec33012c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05eb88e245ed5a4c3b1493c13977fa5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a87772755fe5c07d158",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c9c4b57c5aac273e2a53",
+    "url": "/static/js/2.d85a0743.chunk.js"
+  },
+  {
+    "revision": "9a87772755fe5c07d158",
+    "url": "/static/js/main.023e7bc9.chunk.js"
+  },
+  {
+    "revision": "df1c95787a04a03cc3c3",
+    "url": "/static/js/runtime-main.51ef09f0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
